452cbc3f049ab5089876cb55c8541a6ff5353afc
Oliver Angélil <olivermangelil@gmail.com>
2025-04-02 05:48:39 +0200

    added OpenTofu & Docker Deploy config files (#11)
    * added OpenTofu & Docker Deploy config files
    
    * code checks failed
    
    * .
    
    * .
    
    * removed slack api token

--- opentofu/providers.tf
+++ opentofu/providers.tf
@@ -1,1 +1,10 @@
+terraform {
+  backend "gcs" {
+    prefix = "tofu"
+  }
+}
 
+# Provider configuration (optional, if not using Application Default Credentials)
+provider "google" {
+  project = var.gcp_project_name
+}
---
+++ opentofu/variables.tf
@@ -0,0 +1,25 @@
+# Variable definitions for sensitive data.
+variable "gemini_api_key" {
+  description = "API key for Gemini"
+  type        = string
+  sensitive   = true
+}
+
+variable "billing_account" {
+  description = "Billing account ID"
+  type        = string
+}
+
+variable "org_id" {
+  description = "Organization ID"
+  type        = string
+}
+
+variable "gcp_project_name" {
+  description = "Project Name"
+  type        = string
+}
+
+variable "project_number" {
+  type = string
+}
