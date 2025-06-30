b09ef2478297577460a559eafb64ff151583b284
Vincent Aduuna Frimpong <tossia13@gmail.com>
2025-06-30 18:01:17 +0000

    updated dagster run configurations with workspace.yaml config

--- run_dagster.sh
+++ run_dagster.sh
@@ -6,8 +6,15 @@ mkdir -p "$DAGSTER_HOME"
 if [ ! -f "$DAGSTER_HOME/dagster.yaml" ]; then
     # Copy the root config
     cp /workspaces/AIMS_course/.dagster_home/dagster.yaml "$DAGSTER_HOME/dagster.yaml"
-    # Replace the module line with the user's module
-    sed -i "s|module:.*|module: src.students.$GITHUB_USER.mlflow.era5_temperature_project.era5_pipeline.assets|" "$DAGSTER_HOME/dagster.yaml"
+    # Replace the storage path with the user's path
+    # sed -i "s|module:.*|module: src.students.$GITHUB_USER.mlflow.era5_temperature_project.era5_pipeline.assets|" "$DAGSTER_HOME/dagster.yaml"
 fi
 
-dagster dev
+if [ ! -f "$DAGSTER_HOME/workspace.yaml" ]; then
+    # Copy the root config
+    cp /workspaces/AIMS_course/.dagster_home/workspace.yaml "$DAGSTER_HOME/workspace.yaml"
+    # Replace the module line with the user's module
+    sed -i "s|module_name:.*|module_name: src.students.$GITHUB_USER.mlflow.era5_temperature_project.era5_pipeline.assets|" "$DAGSTER_HOME/workspace.yaml"
+fi
+
+dagster dev -w "$DAGSTER_HOME/workspace.yaml"
