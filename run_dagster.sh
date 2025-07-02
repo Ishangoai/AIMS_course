23943bfa6c53ba4e1980cc1dffbb207b09882de1
Oliver Angélil <olivermangelil@gmail.com>
2025-07-02 16:32:18 +0200

    refactoring (#105)
    * .
    
    * .
    
    * .
    
    * .
    
    * .

--- run_dagster.sh
+++ run_dagster.sh
@@ -4,8 +4,8 @@ echo -e "url: https://cds.climate.copernicus.eu/api\nkey: $CDS_API_KEY" > ~/.cds
 
 if [[ "$GITHUB_USER" == "oliverangelil" || "$GITHUB_USER" == "aduuna" || "$GITHUB_USER" == "cyrille-feu" ]]; then
     echo "Running Dagster for user: example"
-    dagster dev -m "src.students.example.mlflow.era5_temperature_project.era5_pipeline.assets"
+    dagster dev -m "src.students.example.mlflow.era5_temperature_project.era5_pipeline.definitions"
 else
     echo "Attempting to run Dagster for user: $GITHUB_USER"
-    dagster dev -m "src.students.$GITHUB_USER.mlflow.era5_temperature_project.era5_pipeline.assets"
+    dagster dev -m "src.students.$GITHUB_USER.mlflow.era5_temperature_project.era5_pipeline.definitions"
 fi
