bdb6cf439aeee6318e32c09c1d88e15a427410c9
Oliver Angélil <olivermangelil@gmail.com>
2025-07-07 15:55:12 +0000

    .

--- src/students/example/mlflow/era5_temperature_project/era5_pipeline/assets.py
+++ src/students/example/mlflow/era5_temperature_project/era5_pipeline/assets.py
@@ -87,7 +87,6 @@ def raw_netcdf_file(context: dg.AssetExecutionContext) -> str:
 def create_pandas_df(
     context: dg.AssetExecutionContext,
     raw_netcdf_file: str) -> dg.MaterializeResult:
-
     mlflow_client = context.resources.mlflow_tracking
     context.log.info(f"Processing file: {raw_netcdf_file}")
     try:
@@ -120,6 +119,7 @@ def create_pandas_df(
         mlflow_client.log_metric("processed_mean_temperature_k", mean_temp_kelvin)
     context.log.info(f"Pandas DataFrame created. Time steps: {num_time_steps}, Mean Temp (K): {mean_temp_kelvin:.2f}")
     context.log.info("Logged metrics to MLflow.")
+    context.pdb.set_trace()
     return dg.MaterializeResult(
         value=df_agg,
         metadata={
