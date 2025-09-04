0382e28cf9cdcf171960615507512e385f288b56
Vincent Aduuna Frimpong <tossia13@gmail.com>
2025-09-04 18:41:01 +0000

    fixed dagster run failed and nginx run failed (#189)

--- run_dagster.sh
+++ run_dagster.sh
@@ -2,7 +2,7 @@
 
 
 echo "MLflow UI starting on port 5000..."
-mlflow ui --backend-store-uri sqlite:///mlflow_local_tracking.db &
+uv run mlflow ui --backend-store-uri sqlite:///mlflow_local_tracking.db &
 MLFLOW_PID=$!
 
 # Ensure MLflow UI is stopped when the script exits
@@ -10,8 +10,8 @@ trap "kill $MLFLOW_PID 2>/dev/null" EXIT
 
 if [[ "$GITHUB_USER" == "oliverangelil" || "$GITHUB_USER" == "aduuna" || "$GITHUB_USER" == "cyrille-feu" ]]; then
     echo "Running Dagster for user: example"
-    dagster dev -m "src.students.example.dagster.definitions"
+    uv run dagster dev -m "src.students.example.dagster.definitions"
 else
     echo "Attempting to run Dagster for user: $GITHUB_USER"
-    dagster dev -m "src.students.$GITHUB_USER.dagster.definitions"
+    uv run dagster dev -m "src.students.$GITHUB_USER.dagster.definitions"
 fi
