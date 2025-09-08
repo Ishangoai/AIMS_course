c980b155186a5b83fff0a810caab05033065dfeb
Vincent Aduuna Frimpong <tossia13@gmail.com>
2025-09-08 12:54:39 +0000

    Revert "fixed dagster run failed and nginx run failed (#189)"
    This reverts commit 0382e28cf9cdcf171960615507512e385f288b56.

--- run_dagster.sh
+++ run_dagster.sh
@@ -2,7 +2,7 @@
 
 
 echo "MLflow UI starting on port 5000..."
-uv run mlflow ui --backend-store-uri sqlite:///mlflow_local_tracking.db &
+mlflow ui --backend-store-uri sqlite:///mlflow_local_tracking.db &
 MLFLOW_PID=$!
 
 # Ensure MLflow UI is stopped when the script exits
@@ -10,8 +10,8 @@ trap "kill $MLFLOW_PID 2>/dev/null" EXIT
 
 if [[ "$GITHUB_USER" == "oliverangelil" || "$GITHUB_USER" == "aduuna" || "$GITHUB_USER" == "cyrille-feu" ]]; then
     echo "Running Dagster for user: example"
-    uv run dagster dev -m "src.students.example.dagster.definitions"
+    dagster dev -m "src.students.example.dagster.definitions"
 else
     echo "Attempting to run Dagster for user: $GITHUB_USER"
-    uv run dagster dev -m "src.students.$GITHUB_USER.dagster.definitions"
+    dagster dev -m "src.students.$GITHUB_USER.dagster.definitions"
 fi
