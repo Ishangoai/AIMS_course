9c59d6e628e29622489d5ad3cf2dbc55580b3ff6
Vincent Aduuna Frimpong <tossia13@gmail.com>
2025-09-08 13:04:17 +0000

    Fbv (#193)
    * Revert "started dagster before changing location (#191)"
    
    This reverts commit 333d7d56b497e115b7944521e68670d2bd057052.
    
    * Revert "Fbv (#190)"
    
    This reverts commit 901300f7918e464f5c99dd3ca8bdd1482b7ae326.
    
    * Revert "fixed dagster run failed and nginx run failed (#189)"
    
    This reverts commit 0382e28cf9cdcf171960615507512e385f288b56.
    
    * Revert "fixed dockerfile (#188)"
    
    This reverts commit 0335409a9045bee1a4e048ab487d6c07530c6e6c.
    
    * Revert "Dockerfile and nginx configuration to set up nginx as a reverse proxy for the FastAPI application, and modify the entrypoint script to start nginx alongside the FastAPI app. (#187)"
    
    This reverts commit 31dbf955509e82cc8e5f730b729a54a7a96b9c0a.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -e
 
-# Start dagster and mlflow (if present) from project root (/app)
-echo "Launching /app/run_dagster.sh in background..."
-/app/run_dagster.sh &
-RUNDAGSTER_PID=$!
-echo "run_dagster.sh pid=$RUNDAGSTER_PID"
-
 if [ "$DEPLOYMENT_TARGET" = "STUDENT" ]; then
     # Assume you pass in the student identifier, e.g., student1.
     if [ -z "$GITHUB_USER" ]; then
@@ -20,17 +14,4 @@ else
     cd src/students/example
 fi
 
-# Start the FastAPI app in background on port 8000
-echo "Starting FastAPI on port 8000..."
-uv run uvicorn "api.main:app" --host 0.0.0.0 --port 8000 &
-FASTAPI_PID=$!
-
-echo "Waiting for all services to be ready..."
-while ! nc -z 127.0.0.1 8000 || ! nc -z 127.0.0.1 3000 || ! nc -z 127.0.0.1 5000; do
-  sleep 1
-done
-echo "All services are up!"
-
-echo "Starting nginx (will run in foreground)..."
-# exec nginx in the foreground so it becomes PID 1 in the container
-exec nginx -g 'daemon off;'
+exec uv run uvicorn "api.main:app" --host 0.0.0.0 --port 8080
