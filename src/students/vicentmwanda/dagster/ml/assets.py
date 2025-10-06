84f0fc86897b34b158c39f9faeb8a7e6705f3386
vicentmwanda <mwanda.vicent@gmail.com>
2025-10-06 08:18:56 +0000

    Initial commit student folder

--- src/students/vicentmwanda/dagster/ml/assets.py
+++ src/students/vicentmwanda/dagster/ml/assets.py
@@ -6,7 +6,6 @@ import hyperopt
 import mlflow
 import mlflow.sklearn as ms
 import pandas as pd
-import numpy as np
 import xarray as xr
 from sklearn.linear_model import Ridge  # Changed from LinearRegression for tuning
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
