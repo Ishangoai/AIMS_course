03a1d51de715e7c5f56bbf2f02b9157505635dff
Komolafe Elisha <34249609+eak0mo@users.noreply.github.com>
2025-10-07 19:38:49 +0000

    updated student folder

--- src/students/eak0mo/api/main.py
+++ src/students/eak0mo/api/main.py
@@ -42,7 +42,7 @@ def root():
 def hello():
     return {"message": f"Hello from {current_user}!"}
 
-@app.get("/yoruba_hello",summary="Greet the User in Yoruba",description="Returns a greeting message in Yoruba.")
+@app.get("/yoruba_hello", summary="Greet the User in Yoruba", description="Returns a greeting message in Yoruba.")
 def yoruba_hello():
     return{"message": f"Bawo Ni lati {current_user}!"}
 
