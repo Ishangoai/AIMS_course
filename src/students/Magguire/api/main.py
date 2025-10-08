b5e29c020f426c56b0a03d86da093b5368278eb0
Elizabeth Kihungi <98317997+Magguire@users.noreply.github.com>
2025-10-08 05:04:58 +0000

    Updated api desc

--- src/students/Magguire/api/main.py
+++ src/students/Magguire/api/main.py
@@ -11,7 +11,7 @@ from gradioapp.app import app as demo
 from gradioapp.heart_disease_app import heart_app
 
 app = FastAPI(
-    title="AIMS Course API",
+    title="2025 AIMS Course API",
     description=textwrap.dedent("""
     ## Mounted Apps
     ----
