fix rotation

src/students/Toavina00/gradioapp/image_app.py
@@ -1,6 +1,6 @@
 import gradio as gr
 import numpy as np
-from scipy.ndimage import rotate
+from scipy.ndimage import rotate as sp_rotate
 
 
 def img_to_grayscale(image: np.array, to_grayscale: bool):
@@ -28,7 +28,7 @@ def change_contrast(image: np.array, value: float):
 
 
 def rotate_image(image: np.array, radius: float):
-    image= rotate(image, radius, reshape=True)
+    image= sp_rotate(image, radius, reshape=True)
     return image
 
 