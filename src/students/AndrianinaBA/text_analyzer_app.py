39bc35531890efcfdca803b07e22398505e08eb6
Andrianina <andrianinaba@aims.ac.za>
2025-10-11 10:35:27 +0300

    Pull request #0 until all things done (#329)
    * Trying to merge with the repo
    
    * getting started
    
    * And now?
    
    * almost there
    
    * almost there bis
    
    * almost there bis bis

--- src/students/AndrianinaBA/text_analyzer_app.py
+++ src/students/AndrianinaBA/text_analyzer_app.py
@@ -3,11 +3,11 @@
 import gradio as gr
 from gradioapp.utils.text_analyzer import case_converter, text_analyzer, text_reverser
 
-text = "Beams of fire sweep thorough my hearst, thrust of pain increasingly engaged, i am no one now only agony"
-res = case_converter(text=text, option="Title Case")
-print(res)
+# text = "Beams of fire sweep thorough my hearst, thrust of pain increasingly engaged, i am no one now only agony"
+# res = case_converter(text=text, option="Title Case")
+# print(res)
 
-with gr.Blocks(css="body {background: #f2f7ff;}") as text_analyzer_app:
+with gr.Blocks(css="body {background: #f2f7ff;}") as text_analyzer_app_instance:
     gr.Markdown("# AIMS Course: Text Analyzer")
     gr.Markdown("This app provides various text analysis functionalities.")
 
@@ -37,5 +37,10 @@ with gr.Blocks(css="body {background: #f2f7ff;}") as text_analyzer_app:
         analyze_button = gr.Button("Analyze")
         word_count_output = gr.Textbox(label="Word Count")
         char_count_output = gr.Textbox(label="Character Count")
+        average_count_output = gr.Textbox(label="Average Characters per Word")
 
-        analyze_button.click(fn=text_analyzer, inputs=analyze_input, outputs=[word_count_output, char_count_output])
+        analyze_button.click(fn=text_analyzer, inputs=analyze_input, outputs=[word_count_output,
+                                                                             char_count_output,
+                                                                             average_count_output])
+if __name__ == "__main__":
+    text_analyzer_app_instance.launch()
