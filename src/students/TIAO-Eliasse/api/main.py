c8bd161ce0152703114720e3eb3f6dde98b75b3c
Eliasse Tiao <eliasse@aims.ac.za>
2025-10-12 08:29:14 +0200

    initial commit student folder

--- src/students/TIAO-Eliasse/api/main.py
+++ src/students/TIAO-Eliasse/api/main.py
@@ -1,6 +1,9 @@
 import os
 import textwrap
 
+import os
+
+
 import gradio as gr
 from agents.chatbot.llm_gradio import llm_chat
 from api.models import UpdateUserRequest, UserRequest
@@ -9,6 +12,8 @@ from fastapi import FastAPI, HTTPException
 from fastapi.openapi.docs import get_swagger_ui_html
 from gradioapp.app import app as demo
 from gradioapp.heart_disease_app import heart_app
+from gradioapp.Assignment1propoB import gradioImage
+
 
 app = FastAPI(
     title="AIMS Course API",
@@ -18,6 +23,7 @@ app = FastAPI(
     1. [**General Gradio Demo**](/gradio/)
     2. [**Heart Disease Prediction App**](/heart-disease/)
     3. [**Simple LLM Chatbot**](/llm-chat/)
+    4. [**Assignment1**](/gradioImage/)
     -----
     """),
     version="1.0.0",
@@ -128,3 +134,4 @@ def update_user_details(username: str, request: UpdateUserRequest):
 gr.mount_gradio_app(app, demo, path="/gradio")
 gr.mount_gradio_app(app, heart_app, path="/heart-disease")
 gr.mount_gradio_app(app, llm_chat, path="/llm-chat")
+gr.mount_gradio_app(app, gradioImage, path="/gradioImage")
