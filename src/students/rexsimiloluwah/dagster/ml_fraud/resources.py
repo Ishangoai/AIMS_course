f41b697d371dfc7f46af114cae26e9c3e5b65a3d
rexsimiloluwah <adetoyosi2001@gmail.com>
2025-10-17 23:06:37 +0200

    feat: implement initial MLOps pipeline for credit card fraud detection assignment
    This commit introduces the foundational assets for the credit card fraud detection pipeline. It covers the complete data processing lifecycle from ingestion to evaluation, and integrates MLFlow for experiment tracking.
    
    The pipeline now includes hyperparameter tuning for a Random Forest model using nested runs and generates evaluation metrics and a confusion matrix artifact for the test set.
    
    Implemented the following assets and checks:
    
    - raw_fraud_data: Ingests data from the source URL.
    
    - cleaned_fraud_data: Handles duplicates, nulls, and removes unnecessary columns.
    
    - transformed_fraud_data: Applies feature scaling to the amount column.
    
    - data_splits: Creates stratified train/test splits.
    
    - tune_hyperparameters: Performs 3-fold CV to find the best max_depth.
    
    - train_model: Trains the final model using the best hyperparameters.
    
    - evaluate_model: Evaluates the model on the test set and logs metrics/plots to MLFlow.
    
    - raw_data_quality_checks: Validates the raw data for nulls and correct labels.

---
+++ src/students/rexsimiloluwah/dagster/ml_fraud/resources.py
@@ -0,0 +1,27 @@
+
+import dagster as dg
+from dagster_mlflow import mlflow_tracking
+from mlflow.tracking import MlflowClient
+
+from .configs import DataConfig, ExperimentConfig, MLFlowConfig, ModelConfig
+
+# Instantiate all configs
+data_config = DataConfig()
+model_config = ModelConfig()
+mlflow_config = MLFlowConfig()
+experiment_config = ExperimentConfig()
+
+# Define the MLflow resource
+mlflow_resource = mlflow_tracking.configured(
+    {
+        # Point MLflow to use the local SQLite database
+        "mlflow_tracking_uri": mlflow_config.tracking_uri,
+        "experiment_name": experiment_config.experiment_name
+    }
+)
+
+
+# Raw MlflowClient for advanced API access (transition_model_version_stage,....)
+@dg.resource
+def mlflow_client(_):
+    return MlflowClient(tracking_uri=mlflow_config.tracking_uri)
