5d767c2e0e5082aa475ae197a9be48a87d7faa3f
AndrianinaBA <andrianinaba@aims.ac.za>
2025-10-17 16:43:38 +0200

    Added some assets and getting started again

--- src/students/AndrianinaBA/dagster/definitions.py
+++ src/students/AndrianinaBA/dagster/definitions.py
@@ -52,7 +52,7 @@ ml_job = dg.define_asset_job(
 
 ml_fraud_job = dg.define_asset_job(
     name="ml_fraud_detection_job",
-    selection=dg.AssetSelection.groups("ml_fraud_ingest")
+    selection=dg.AssetSelection.groups("ml_fraud_ingest", "ml_fraud_model"),
 )
 
 era5_daily_schedule = dg.ScheduleDefinition(
