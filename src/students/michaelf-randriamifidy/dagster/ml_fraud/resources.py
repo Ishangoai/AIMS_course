caf616c1df9626f1e517ddc1d26a593741e1c949
RadoFitiavana <radof@aims.ac.za>
2025-10-17 20:32:59 +0200

    create downloader utility in utils.py and reimplement FraudResourceConfig

--- src/students/michaelf-randriamifidy/dagster/ml_fraud/resources.py
+++ src/students/michaelf-randriamifidy/dagster/ml_fraud/resources.py
@@ -1,4 +1,11 @@
 import dagster as dg
 
+from utils import ClientDownloader
+
+
 class FraudResourceConfig(dg.ConfigurableResource):
     data_url: str = "https://raw.githubusercontent.com/aduuna/Kaggle-Data-Credit-Card-Fraud-Detection/master/samplecreditcard.csv"
+
+    @property
+    def client(self) -> ClientDownloader:
+        return ClientDownloader(url=self.data_url)
---
+++ src/students/michaelf-randriamifidy/dagster/ml_fraud/utils.py
@@ -0,0 +1,31 @@
+import os
+
+import requests
+
+
+class ClientDownloader:
+    def __init__(self, url=None):
+        self.url = url
+
+    def download_and_save(self, output_filename):
+        if not isinstance(self.url, str) or not self.url.strip():
+            raise ValueError("URL is not set or invalid.")
+
+        if not isinstance(output_filename, str) or not output_filename.strip():
+            raise ValueError("Invalid output filename.")
+
+        try:
+            response = requests.get(self.url, stream=True)
+            response.raise_for_status()
+
+        except requests.exceptions.RequestException as e:
+            raise RuntimeError(f"Request failed: {e}")
+
+        try:
+            with open(os.getcwd() + output_filename, "wb") as f:
+                for chunk in response.iter_content(chunk_size=8192):
+                    if chunk:
+                        f.write(chunk)
+
+        except IOError as e:
+            raise RuntimeError(f"Failed to write file: {e}")
