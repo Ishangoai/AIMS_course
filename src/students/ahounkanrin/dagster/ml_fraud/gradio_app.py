a2402608ac74d8f08b74c8d478e39b82393c6f28
ahounkanrin <anicet@aims.ac.za>
2025-10-19 12:40:17 +0000

    ruff checks done

--- src/students/ahounkanrin/dagster/ml_fraud/gradio_app.py
+++ src/students/ahounkanrin/dagster/ml_fraud/gradio_app.py
@@ -26,7 +26,7 @@ model = mlflow.pyfunc.load_model(model_uri)
 def predict_fraud(*input_features):
     input_features = list(input_features)
     input_features = [float(x) for x in input_features]
-    feature_names = ["V1", "V2", "V3", "V4", "V5", "V6", "V7", "V8", "V9", "V10", 
+    feature_names = ["V1", "V2", "V3", "V4", "V5", "V6", "V7", "V8", "V9", "V10",
                                      "V11", "V12", "V13", "V14", "V15", "V16", "V17",
                                      "V18", "V19", "V20", "V21", "V22", "V23", "V24",
                                      "V25", "V26", "V27", "V28", "Amount"]
