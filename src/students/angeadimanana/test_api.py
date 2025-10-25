1a8e5949be66d93410572b612d03c42386dadd07
Notahinjanahary Ange ADIMANANA <angenotahin@aims.ac.za>
2025-10-25 22:53:03 +0200

    fix error on ruff check

--- src/students/angeadimanana/test_api.py
+++ src/students/angeadimanana/test_api.py
@@ -1,8 +1,8 @@
-from google import genai
-from google.genai import types
 import os
 
-api_key= os.getenv("GEMINI_API_KEY")
+from google import genai
+
+api_key = os.getenv("GEMINI_API_KEY")
 client = genai.Client(api_key="AIzaSyConloRzNoTi14lPOp9fr7G_95VUxu19EA")
 response = client.models.generate_content(
     model='gemini-2.5-flash', contents='What is the capital of France?'
